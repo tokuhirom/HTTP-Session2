976a684762a4cf47542285fe5baaa5f46514c244
tokuhirom <tokuhirom@gmail.com>
2013-11-11 17:22:03 +0900

    jshint clean.

--- js/xsrf-token.js
+++ js/xsrf-token.js
@@ -1,29 +1,31 @@
-$(function () {
-    "use strict";
+(function (document, $) {
+    $(function () {
+        "use strict";
 
-    var xsrf_token = getXSRFToken();
-    $("form").each(function () {
-        var form = $(this);
-        var method = form.attr('method');
-        if (method === 'get' || method === 'GET') {
-            return;
-        }
-
-        var input = $(document.createElement('input'));
-        input.attr('type',  'hidden');
-        input.attr('name',  'XSRF-TOKEN');
-        input.attr('value',  xsrf_token);
-        form.prepend(input);
-    });
-
-    function getXSRFToken() {
-        var cookies = document.cookie.split(/\s*;\s*/);
-        for (var i=0,l=cookies.length; i<l; i++) {
-            var matched = cookies[i].match(/^XSRF-TOKEN=(.*)$/);
-            if (matched) {
-                return matched[1];
+        var xsrf_token = getXSRFToken();
+        $("form").each(function () {
+            var form = $(this);
+            var method = form.attr('method');
+            if (method === 'get' || method === 'GET') {
+                return;
             }
+
+            var input = $(document.createElement('input'));
+            input.attr('type',  'hidden');
+            input.attr('name',  'XSRF-TOKEN');
+            input.attr('value',  xsrf_token);
+            form.prepend(input);
+        });
+
+        function getXSRFToken() {
+            var cookies = document.cookie.split(/\s*;\s*/);
+            for (var i=0,l=cookies.length; i<l; i++) {
+                var matched = cookies[i].match(/^XSRF-TOKEN=(.*)$/);
+                if (matched) {
+                    return matched[1];
+                }
+            }
+            return undefined;
         }
-        return undefined;
-    }
-});
+    });
+})(document, jQuery);
