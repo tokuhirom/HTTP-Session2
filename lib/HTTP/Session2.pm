8c0caf9d4637bcc23af38cfe95d27687d7f20344
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-07-28 13:10:10 +0900

    Checking in changes prior to tagging of version 1.00.
    Changelog diff is:
    
    diff --git Changes Changes
    index 548783b..e4f103f 100644
    --- Changes
    +++ Changes
    @@ -2,6 +2,22 @@ Revision history for Perl extension HTTP-Session2
    
     {{$NEXT}}
    
    +1.00 2014-07-28T04:09:47Z
    +
    +    [INCOMPATIBLE CHANGE]
    +    - I changed HMAC strategy on ServerSide mode.
    +      Previous version uses
    +
    +        hmac_hex(data: $session_id, key: $secret)
    +
    +      New version is:
    +
    +        hmac_hex(data: $secret, key: $session)
    +
    +      This version is even secure. But, it's not a critical issue.
    +
    +      I think this change won't break your code.
    +
     0.05 2014-03-18T18:52:37Z
    
         - use Cookie::Baker for generating cookie string for Plack response object.

--- lib/HTTP/Session2.pm
+++ lib/HTTP/Session2.pm
@@ -3,7 +3,7 @@ use 5.008005;
 use strict;
 use warnings;
 
-our $VERSION = "0.05";
+our $VERSION = "1.00";
 
 1;
 __END__
--- lib/HTTP/Session2/ServerStore.pm
+++ lib/HTTP/Session2/ServerStore.pm
@@ -4,7 +4,7 @@ use warnings;
 use utf8;
 use 5.008_001;
 
-our $VERSION = "0.05";
+our $VERSION = "1.00";
 
 use Carp ();
 use Digest::HMAC;
