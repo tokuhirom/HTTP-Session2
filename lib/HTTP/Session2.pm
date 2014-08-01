212a160e305b76439e00016a3f55b0bd9f4d42e5
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-08-01 23:02:56 +0900

    Checking in changes prior to tagging of version 1.06.
    Changelog diff is:
    
    diff --git Changes Changes
    index 3c3c2d9..24da7de 100644
    --- Changes
    +++ Changes
    @@ -2,6 +2,12 @@ Revision history for Perl extension HTTP-Session2
    
     {{$NEXT}}
    
    +1.06 2014-08-01T14:00:33Z
    +
    +    [BUG FIX]
    +    - Random string generator doesn't works if /dev/urandom is not available.
    +      This issue was introduced at 1.02.
    +
     1.05 2014-08-01T11:20:13Z
    
         - Show warnings if the secret string is too short.

--- lib/HTTP/Session2.pm
+++ lib/HTTP/Session2.pm
@@ -3,7 +3,7 @@ use 5.008005;
 use strict;
 use warnings;
 
-our $VERSION = "1.05";
+our $VERSION = "1.06";
 
 1;
 __END__
--- lib/HTTP/Session2/ServerStore.pm
+++ lib/HTTP/Session2/ServerStore.pm
@@ -4,7 +4,7 @@ use warnings;
 use utf8;
 use 5.008_001;
 
-our $VERSION = "1.05";
+our $VERSION = "1.06";
 
 use Carp ();
 use Digest::HMAC;
